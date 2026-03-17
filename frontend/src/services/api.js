bc451792dddc4df651f17b4082167ca2e5d6c70b
sameerturkar <sameerturkar9@gmail.com>
2026-03-17 18:26:26 +0530

    frontend api updated for vercel

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api';
+const BASE_URL = 'https://image-steganography-o6mn.onrender.com';
 
 const api = axios.create({
   baseURL: BASE_URL,
